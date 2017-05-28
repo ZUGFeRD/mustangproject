b640108b2b371b62c556d16bd6b4ba805d4e39ce
Jochen Stärk <jstaerk@usegroup.de Jochen Staerk>
2017-05-28 12:51:08 +0200

    corrected merge error

--- src/main/java/org/mustangproject/ZUGFeRD/ZUGFeRDConformanceLevel.java
+++ src/main/java/org/mustangproject/ZUGFeRD/ZUGFeRDConformanceLevel.java
@@ -1,9 +1,5 @@
 package org.mustangproject.ZUGFeRD;
 
 public enum ZUGFeRDConformanceLevel {
-<<<<<<< HEAD
     BASIC, COMFORT, EXTENDED;
-=======
-    BASIC, COMFORT, EXTENDED
->>>>>>> a6c972eb3cdfdebf0374a2e87a48bd14b463ff78
 }
