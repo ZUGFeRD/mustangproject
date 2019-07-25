ce5af7b012ca2182c3d51ac6f54d136265bc5324
Stefan Schmiedl <s@xss.de>
2019-07-25 16:55:28 +0200

    complete profile options for v1
    see "Das ZUGFeRD-Format_1p0", p. 98, lines 1982 ff.

--- src/main/java/org/mustangproject/ZUGFeRD/model/DocumentContextParameterTypeConstants.java
+++ src/main/java/org/mustangproject/ZUGFeRD/model/DocumentContextParameterTypeConstants.java
@@ -19,7 +19,7 @@
 package org.mustangproject.ZUGFeRD.model;
 
 public class DocumentContextParameterTypeConstants {
+	public static final String BASIC = "urn:ferd:CrossIndustryDocument:invoice:1p0:basic";
 	public static final String COMFORT = "urn:ferd:CrossIndustryDocument:invoice:1p0:comfort";
 	public static final String EXTENDED = "urn:ferd:CrossIndustryDocument:invoice:1p0:extended";
-	// this does not look complete :-/
 }
