0708bcf91e92d7457c8d7b1b3240a5316d3d87e1
Jochen Stärk <jstaerk@usegroup.de Jochen Staerk>
2019-08-09 18:46:00 +0200

    re-enable pretty print

--- src/main/java/org/mustangproject/ZUGFeRD/ZUGFeRD2PullProvider.java
+++ src/main/java/org/mustangproject/ZUGFeRD/ZUGFeRD2PullProvider.java
@@ -124,7 +124,7 @@ public class ZUGFeRD2PullProvider implements IXMLProvider, IProfileProvider {
 
 		byte[] res = zugferdData;
 		
-		/*StringWriter sw = new StringWriter();
+		StringWriter sw = new StringWriter();
 		Document document=null;
 		try {
 			document = DocumentHelper.parseText(new String(zugferdData));
@@ -140,7 +140,7 @@ public class ZUGFeRD2PullProvider implements IXMLProvider, IProfileProvider {
 		} catch (IOException e) {
 			Logger.getLogger(ZUGFeRD2PullProvider.class.getName()).log(Level.SEVERE, null, e);
 		}
-		*/
+		
 		return res;
 		
 	}
