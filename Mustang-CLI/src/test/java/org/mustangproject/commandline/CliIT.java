1ec64c61103a637494eb602174c255d5187bafe4
Ulrich Zielhofer <u.zielhofer@sanacorp.de>
2021-12-17 08:48:04 +0100

    solves #227: create both non-shaded and shaded jar for library, while not breaking CLI

---
+++ Mustang-CLI/src/test/java/org/mustangproject/commandline/CliIT.java
@@ -0,0 +1,47 @@
+package org.mustangproject.commandline;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
+
+public class CliIT {
+
+	@Test
+	public void testCii2Ubl() throws Exception {
+		Path output = Paths.get("target/ubl.xml");
+		Files.deleteIfExists(output);
+		Path jar = Files.newDirectoryStream(Paths.get("target"), "Mustang-CLI-*.jar").iterator().next();
+		ProcessBuilder pb = new ProcessBuilder("java", "-jar", jar.toString(),
+				"--action", "ubl", "--source", "src/test/resources/cii.xml", "--out",
+				output.toString());
+		pb.redirectErrorStream(true);
+		Process process = pb.start();
+		String result = getOutput(process);
+		process.waitFor(10, TimeUnit.SECONDS);
+		if (!result.isEmpty()) {
+			System.out.println(result);
+		}
+		assertTrue(new String(Files.readAllBytes(output), StandardCharsets.UTF_8).contains("Invoice"));
+	}
+
+	private String getOutput(Process process) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		StringBuilder builder = new StringBuilder();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			builder.append(line);
+			builder.append(System.getProperty("line.separator"));
+		}
+		return builder.toString();
+	}
+
+}
