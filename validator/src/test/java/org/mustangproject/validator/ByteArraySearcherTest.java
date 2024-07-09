7483a0e0d63835e148f0db9f4a055e5bec64dea6
Philip Helger <philip@helger.com>
2024-07-09 17:18:36 +0200

    Extended ByteArarySearcher API

---
+++ validator/src/test/java/org/mustangproject/validator/ByteArraySearcherTest.java
@@ -0,0 +1,24 @@
+package org.mustangproject.validator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.charset.StandardCharsets;
+
+import org.junit.Test;
+
+public class ByteArraySearcherTest {
+  @Test
+  public void testIndexOf () {
+    byte[] haystack = "Hello World".getBytes (StandardCharsets.ISO_8859_1);
+    assertEquals (0, ByteArraySearcher.indexOf (haystack, new byte[] {'H'}));
+    assertEquals (1, ByteArraySearcher.indexOf (haystack, new byte[] {'e'}));
+    assertEquals (0, ByteArraySearcher.indexOf (haystack, new byte[] {'H', 'e'}));
+    assertEquals (0, ByteArraySearcher.indexOf (haystack, new byte[] {'H', 'e'}));
+    assertEquals (0, ByteArraySearcher.indexOf (haystack, new byte[] {'H', 'e', 'l', 'l'}));
+    assertEquals (0, ByteArraySearcher.indexOf (haystack, haystack));
+    assertEquals (0, ByteArraySearcher.indexOf (haystack, new byte[0]));
+    assertEquals (-1, ByteArraySearcher.indexOf (haystack, new byte[] {'a'}));
+    assertEquals (-1, ByteArraySearcher.indexOf (haystack, new byte[] {'h'}));
+    assertEquals (-1, ByteArraySearcher.indexOf (haystack, new byte[] {'r', 'o'}));
+  }
+}
