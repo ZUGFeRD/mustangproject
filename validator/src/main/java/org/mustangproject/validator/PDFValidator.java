ad8fc7e7fff0e5a392ec930044e0a702215ff395
Philip Helger <philip@helger.com>
2024-07-09 19:32:54 +0200

    Cleansing

--- validator/src/main/java/org/mustangproject/validator/PDFValidator.java
+++ validator/src/main/java/org/mustangproject/validator/PDFValidator.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
