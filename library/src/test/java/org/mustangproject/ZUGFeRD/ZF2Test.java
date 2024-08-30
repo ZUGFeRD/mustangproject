189decbe28be7178e0e3e9a0e1468af77f565931
Jens Nober <noj@gff.de>
2024-08-30 09:22:47 +0200

    Issue #458 Allow multiple PaymentTerms for in Invoice

--- library/src/test/java/org/mustangproject/ZUGFeRD/ZF2Test.java
+++ library/src/test/java/org/mustangproject/ZUGFeRD/ZF2Test.java
@@ -243,7 +243,7 @@ public class ZF2Test extends MustangReaderTestCase {
 		try {
 			assertEquals(zi.getVersion(), 2);
 		} catch (final Exception e) {
-			// TCalODO Auto-generated catch block
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
