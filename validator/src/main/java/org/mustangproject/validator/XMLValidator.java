451bd95f763445909f9b4729e232c55d372628fc
jstaerk <jstaerk@usegroup.de>
2024-09-22 15:36:41 +0200

    closes #487

--- validator/src/main/java/org/mustangproject/validator/XMLValidator.java
+++ validator/src/main/java/org/mustangproject/validator/XMLValidator.java
@@ -190,7 +190,7 @@ public class XMLValidator extends Validator {
 				boolean isEN16931 = false;
 				boolean isExtended = false;
 				boolean isXRechnung = false;
-				String currentZFVersionDir = "ZF_221";
+				String currentZFVersionDir = "ZF_230";
 				int mainSchematronSectionErrorTypeCode=4;
 				String xsltFilename = null;
 				// urn:ferd:CrossIndustryDocument:invoice:1p0:extended,
