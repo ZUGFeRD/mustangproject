500a25c3bd8bcf418adc8cdbc0d3d6a8d1563d1e
Sebastian Sieber <sieber@weclapp.com>
2024-11-05 12:32:43 +0100

    Add ServiceLogisticsCharge to visualization
    (cherry picked from commit dd8a95ef4bf9e8bc47b411434136995a2df8b6f0)

--- library/src/test/java/org/mustangproject/ZUGFeRD/VisualizationTest.java
+++ library/src/test/java/org/mustangproject/ZUGFeRD/VisualizationTest.java
@@ -74,6 +74,47 @@ public class VisualizationTest extends ResourceCase {
 			fail("TransformerException should not happen: " + e.getMessage());
 		} catch (IOException e) {
 			fail("IOException should not happen: " + e.getMessage());
+		}
+
+
+		assertNotNull(result);
+		// Reading ZUGFeRD
+		assertEquals(expected, result);
+	}
+
+	public void testCIIVisualizationExtended() {
+
+		// the writing part
+		String sourceFilename = "factur-x-extended.xml";
+		File CIIinputFile = getResourceAsFile(sourceFilename);
+
+		String expected = null;
+		String result = null;
+		try {
+			ZUGFeRDVisualizer zvi = new ZUGFeRDVisualizer();
+			/* remove file endings so that tests can also pass after checking
+			   out from git with arbitrary options (which may include CSRF changes)
+			 */
+			result = zvi.visualize(CIIinputFile.getAbsolutePath(), ZUGFeRDVisualizer.Language.DE).replace("\r", "").replace("\n", "")
+				.replace("\t", "")
+				.replace(" ", "");
+
+			File expectedResult = getResourceAsFile("factur-x-vis-extended.de.html");
+			expected = new String(Files.readAllBytes(expectedResult.toPath()), StandardCharsets.UTF_8)
+				.replace("\r", "")
+				.replace("\n", "")
+				.replace("\t", "")
+				.replace(" ", "");
+			// remove linebreaks as well...
+
+		} catch (UnsupportedOperationException e) {
+			fail("UnsupportedOperationException should not happen: " + e.getMessage());
+		} catch (IllegalArgumentException e) {
+			fail("IllegalArgumentException should not happen: " + e.getMessage());
+		} catch (TransformerException e) {
+			fail("TransformerException should not happen: " + e.getMessage());
+		} catch (IOException e) {
+			fail("IOException should not happen: " + e.getMessage());
 		} catch (ParserConfigurationException e) {
 			fail("ParserConfigurationException should not happen: " + e.getMessage());
 		} catch (SAXException e) {
@@ -124,6 +165,7 @@ public class VisualizationTest extends ResourceCase {
 		}
 
 
+
 		assertNotNull(result);
 		// Reading ZUGFeRD
 		assertEquals(expected, result);
@@ -141,10 +183,18 @@ public class VisualizationTest extends ResourceCase {
 			/* remove file endings so that tests can also pass after checking
 			   out from git with arbitrary options (which may include CSRF changes)
 			 */
-			result = zvi.visualize(UBLinputFile.getAbsolutePath(), ZUGFeRDVisualizer.Language.EN).replace("\r", "").replace("\n", "").replace("\t", "").replace(" ", "");
+			result = zvi.visualize(UBLinputFile.getAbsolutePath(), ZUGFeRDVisualizer.Language.EN)
+				.replace("\r", "")
+				.replace("\n", "")
+				.replace("\t", "")
+				.replace(" ", "");
 
 			File expectedResult = getResourceAsFile("factur-x-vis-ubl-creditnote.en.html");
-			expected = new String(Files.readAllBytes(expectedResult.toPath()), StandardCharsets.UTF_8).replace("\r", "").replace("\n", "").replace("\t", "").replace(" ", "");
+			expected = new String(Files.readAllBytes(expectedResult.toPath()), StandardCharsets.UTF_8)
+				.replace("\r", "")
+				.replace("\n", "")
+				.replace("\t", "")
+				.replace(" ", "");
 			// remove linebreaks as well...
 
 		} catch (UnsupportedOperationException e) {
@@ -162,11 +212,11 @@ public class VisualizationTest extends ResourceCase {
 		}
 
 
+
 		assertNotNull(result);
 		// Reading ZUGFeRD
 		assertEquals(expected, result);
 	}
-
 
 	public void testUBLVisualizationBasic() {
 
@@ -198,10 +248,6 @@ public class VisualizationTest extends ResourceCase {
 			fail("TransformerException should not happen: " + e.getMessage());
 		} catch (IOException e) {
 			fail("IOException should not happen: " + e.getMessage());
-		} catch (ParserConfigurationException e) {
-			fail("ParserConfigurationException should not happen: " + e.getMessage());
-		} catch (SAXException e) {
-			fail("SAXException should not happen: " + e.getMessage());
 		}
 
 
