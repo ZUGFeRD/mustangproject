f7e7b91224b7702d6ac579ec38994a3eafe29347
Adrian-Devries <Adrian-Devries@users.noreply.github.com>
2025-01-13 00:47:07 +0100

    Re-apply changes for PR#644

--- library/src/main/java/org/mustangproject/ClasspathResolverURIAdapter.java
+++ library/src/main/java/org/mustangproject/ClasspathResolverURIAdapter.java
@@ -7,31 +7,35 @@ import java.net.URL;
 import org.apache.fop.apps.io.ResourceResolverFactory;
 import org.apache.xmlgraphics.io.Resource;
 import org.apache.xmlgraphics.io.ResourceResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ClasspathResolverURIAdapter implements ResourceResolver {
 
-	private final ResourceResolver wrapped;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClasspathResolverURIAdapter.class);
+    private final ResourceResolver wrapped;
 
 
-	public ClasspathResolverURIAdapter() {
-		this.wrapped = ResourceResolverFactory.createDefaultResourceResolver();
-	}
+    public ClasspathResolverURIAdapter() {
+        this.wrapped = ResourceResolverFactory.createDefaultResourceResolver();
+    }
 
 
-	@Override
-	public Resource getResource(URI uri) throws IOException {
-		if (uri.getScheme().equals("classpath")) {
-			URL url = getClass().getClassLoader().getResource(uri.getSchemeSpecificPart());
+    @Override
+    public Resource getResource(URI uri) throws IOException {
+        LOGGER.debug("public Resource getResource(URI uri='{}')", uri);
+        if (uri.getScheme().equals("classpath")) {
+            URL url = getClass().getClassLoader().getResource(uri.getSchemeSpecificPart());
 
-			return new Resource(url.openStream());
-		} else {
-			return wrapped.getResource(uri);
-		}
-	}
+            return new Resource(url.openStream());
+        } else {
+            return wrapped.getResource(uri);
+        }
+    }
 
-	@Override
-	public OutputStream getOutputStream(URI uri) throws IOException {
-		return wrapped.getOutputStream(uri);
-	}
+    @Override
+    public OutputStream getOutputStream(URI uri) throws IOException {
+        return wrapped.getOutputStream(uri);
+    }
 
 }
