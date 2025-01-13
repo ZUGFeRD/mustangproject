9677ccecff04c49102c30af96b2a724704c24703
Adrian-Devries <Adrian-Devries@users.noreply.github.com>
2025-01-13 17:04:20 +0100

    Fix leading white space

--- library/src/main/java/org/mustangproject/Exceptions/ArithmeticException.java
+++ library/src/main/java/org/mustangproject/Exceptions/ArithmeticException.java
@@ -6,11 +6,11 @@ import java.text.ParseException;
  * will be thrown if an invoice cannot be reproduced numerically
  */
 public class ArithmeticException extends ParseException {
-    @SuppressWarnings("unused")
-    public ArithmeticException() {
-        this("");
-    }
-    public ArithmeticException(final String msg) {
-        super("Could not reproduce the invoice. " + msg, 0);
-    }
+	@SuppressWarnings("unused")
+	public ArithmeticException() {
+		this("");
+	}
+	public ArithmeticException(final String msg) {
+		super("Could not reproduce the invoice. " + msg, 0);
+	}
 }
--- library/src/main/java/org/mustangproject/Exceptions/StructureException.java
+++ library/src/main/java/org/mustangproject/Exceptions/StructureException.java
@@ -6,7 +6,7 @@ import java.text.ParseException;
  * will be thrown if an invoice cannot be read
  */
 public class StructureException extends ParseException {
-    public StructureException(String message, int line) {
-        super(message, line);
-    }
+	public StructureException(String message, int line) {
+		super(message, line);
+	}
 }
