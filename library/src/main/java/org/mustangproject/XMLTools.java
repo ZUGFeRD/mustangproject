ebc26a85dc3b9b53e6e363a110d1b274b6be0d6f
langfr <frank.langelage@outlook.de>
2025-02-28 11:03:14 +0000

    Fix NPE in XMLTools if arg toParse is null.

--- library/src/main/java/org/mustangproject/XMLTools.java
+++ library/src/main/java/org/mustangproject/XMLTools.java
@@ -130,7 +130,9 @@ public class XMLTools extends XMLWriter {
 	 */
 	public static Date tryDate(String toParse) {
 		SimpleDateFormat formatter = null;
-		if (toParse.contains("-")) {
+		if (toParse == null) {
+			return null;
+		} else if (toParse.contains("-")) {
 			// from ubl
 			 formatter = new SimpleDateFormat("yyyy-MM-dd");
 		} else {
