cd878b65a6476d333c1bdd1ca7bd542519c238f7
normangyhra <norman.gyhra@cologne-intelligence.de>
2026-09-03 17:58:25 +0200

    satisfy checkstyle

--- library/src/test/java/org/mustangproject/ZUGFeRD/ZF2ZInvoiceImporterTest.java
+++ library/src/test/java/org/mustangproject/ZUGFeRD/ZF2ZInvoiceImporterTest.java
@@ -46,7 +46,6 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
